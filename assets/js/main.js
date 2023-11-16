4738afe3eb8efa41ff3b480750a8979b7023a7f0
Gabriel Robertson <gabrielrobertson.s@gmail.com>
2023-11-16 11:37:59 -0300

    scroll de menu navegação bloquado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,19 @@
 
 const abrir = document.querySelector('.open');
+const body = document.querySelector('body');
 const fechar = document.querySelector('.close');
 const Abrindo = document.querySelector('nav');
 
 abrir.addEventListener ('mouseover', function () {
     console.log('Abrindo Menu');
     Abrindo.classList.add('menuAberto');
+    body.classList.add('scroll')
+
 })
 
 fechar.addEventListener('mouseover', function(){
     Abrindo.classList.remove('menuAberto')
+    body.classList.remove('scroll')
 })
 
 
