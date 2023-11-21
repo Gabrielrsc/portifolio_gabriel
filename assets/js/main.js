a70b757f8d7042e6717614c55e062df9f1afbe83
Gabriel Robertson <gabrielrobertson.s@gmail.com>
2023-11-20 21:08:54 -0300

    responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@ const abrir = document.querySelector('.open');
 const body = document.querySelector('body');
 const fechar = document.querySelector('.close');
 const Abrindo = document.querySelector('nav');
+const profissional = document.querySelector('#profissional')
+const areasProfissional = document.querySelector('.profissional')
+const academico = document.querySelector('#academico')
+const areaAcademica = document.querySelector('.academico')
 
 abrir.addEventListener ('mouseover', function () {
     console.log('Abrindo Menu');
@@ -16,6 +20,19 @@ fechar.addEventListener('mouseover', function(){
     body.classList.remove('scroll')
 })
 
+profissional.addEventListener('click', ()=>{
+    areasProfissional.classList.add('active')
+    areaAcademica.classList.remove('active')
+    profissional.classList.add('active')
+    academico.classList.remove('active')
+})
+
+academico.addEventListener('click', ()=>{
+    areasProfissional.classList.remove('active')
+    areaAcademica.classList.add('active')
+    academico.classList.add('active')
+    profissional.classList.remove('active')
+})
 
 
 
