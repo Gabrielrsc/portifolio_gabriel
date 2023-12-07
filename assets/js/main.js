a190e40c5de529a323b267ffe7785f6d0b7bf391
Gabriel Robertson <gabrielrobertson.s@gmail.com>
2023-12-07 13:36:27 -0300

    atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,34 @@
-
 const abrir = document.querySelector('.open');
 const body = document.querySelector('body');
 const fechar = document.querySelector('.close');
-const Abrindo = document.querySelector('nav');
-const profissional = document.querySelector('#profissional')
-const areasProfissional = document.querySelector('.profissional')
-const academico = document.querySelector('#academico')
-const areaAcademica = document.querySelector('.academico')
+const abrindo = document.querySelector('nav');
+const profissional = document.querySelector('#profissional');
+const areasProfissional = document.querySelector('.profissional');
+const academico = document.querySelector('#academico');
+const areaAcademica = document.querySelector('.academico');
+const claro = document.querySelector('.temaClaro');
 
-const claro = document.querySelector('.temaClaro')
+function openMenu() {
+    abrindo.classList.toggle('menuAberto');
+    body.classList.toggle('scroll'); // Adiciona ou remove a classe 'scroll' no body
+    
+}
 
-
-claro.addEventListener('click', ()=>{
-    document.documentElement.styleProperty("--color-primaria", "--color-claro")
-    console.log('oiiiii')
-})
-
-abrir.addEventListener ('mouseover', function () {
-    console.log('Abrindo Menu');
-    Abrindo.classList.add('menuAberto');
-    body.classList.add('scroll')
-
-})
-
-fechar.addEventListener('mouseover', function(){
-    Abrindo.classList.remove('menuAberto')
-    body.classList.remove('scroll')
-})
-
-profissional.addEventListener('click', ()=>{
-    areasProfissional.classList.add('active')
-    areaAcademica.classList.remove('active')
-    profissional.classList.add('active')
-    academico.classList.remove('active')
-})
-
-academico.addEventListener('click', ()=>{
-    areasProfissional.classList.remove('active')
-    areaAcademica.classList.add('active')
-    academico.classList.add('active')
-    profissional.classList.remove('active')
-})
+abrir.addEventListener('click', openMenu);
+fechar.addEventListener('click', openMenu)
 
 
 
-// const nav = document.querySelector('#header nav')
-// const toggle = document.querySelectorAll('nav .toggle')
+profissional.addEventListener('click', () => {
+    areasProfissional.classList.add('active');
+    areaAcademica.classList.remove('active');
+    profissional.classList.add('active');
+    academico.classList.remove('active');
+});
 
-// for (const element of toggle) {
-//   element.addEventListener('click', function () {
-//     nav.classList.toggle('show')
-//   })
-// }
+academico.addEventListener('click', () => {
+    areasProfissional.classList.remove('active');
+    areaAcademica.classList.add('active');
+    academico.classList.add('active');
+    profissional.classList.remove('active');
+});
